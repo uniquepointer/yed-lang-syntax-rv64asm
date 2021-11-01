77eebe8deabe6a79a1252ec87aadb3c8fdbc927e
uniquepointer <uniquepointer@mailbox.org>
2021-11-01 00:53:17 -0500

    i added missing kw

--- rv64asm.c
+++ rv64asm.c
@@ -23,6 +23,13 @@ yed_plugin_boot(yed_plugin* self)
 
     char* kwds[] =
     {
+        "addi4spn",  "fld",        "lw",        "flw",      "ld",
+        "fsd",       "sw",         "fsw",       "sd",       "nop",
+        "addi",      "addiw",      "li",        "addi16sp", "lui",
+        "srli",      "srai",       "andi",      "sub",      "xor",
+        "or",        "and",        "subw",      "addw",     "slli",
+        "fldsp",     "lwsp",       "flwsp",     "ldsp",     "mv",
+        "add",       "fsdsp",      "swsp",      "fswsp",    "sdsp",
         "lui",       "auipc",      "addi",      "slti",     "sltiu",
         "xori",      "ori",        "andi",      "slli",     "srli",
         "srai",      "add",        "sub",       "sll",      "slt",
@@ -45,22 +52,23 @@ yed_plugin_boot(yed_plugin* self)
         "fclass.d",  "fcvt.w.d",   "fcvt.wu.d", "fcvt.d.w", "fcvt.d.wu",
         "flw",       "fsw",        "fld",       "fsd",      "fcvt.l.s",
         "fcvt.lu.s", "fcvt.s.l",   "fcvt.s.lu",
-
     };
     char* special_kwds[] =
     {
-        "x0",  "x1",  "x2",  "x3",  "x4",  "x5",  "x6",  "x7",  "x8",  "x9",
-        "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x18", "x19",
-        "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x29",
-        "x30", "x31", "f0",  "f1",  "f2",  "f3",  "f4",  "f5",  "f6",  "f7",
-        "f8",  "f9",  "f10", "f11", "f12", "f13", "f14", "f15", "f16", "f17",
-        "f18", "f19", "f20", "f21", "f22", "f23", "f24", "f25", "f26", "f27",
-        "f28", "f29", "f30", "f31",
+        "zero", "ra",  "sp",   "gp",   "tp",  "t0",  "t1",   "t2",
+        "s0",   "s1",  "a0",   "a1",   "a2",  "a3",  "a4",   "a5",
+        "a6",   "a7",  "s2",   "s3",   "s4",  "s5",  "s6",   "s7",
+        "s8",   "s9",  "s10",  "s11",  "t3",  "t4",  "t5",   "t6",
+        "ft0",  "ft1", "ft2",  "ft3",  "ft4", "ft5", "ft6",  "ft7",
+        "fs0",  "fs1", "fa0",  "fa1",  "fa2", "fa3", "fa4",  "fa5",
+        "fa6",  "fa7", "fs2",  "fs3",  "fs4", "fs5", "fs6",  "fs7",
+        "fs8",  "fs9", "fs10", "fs11", "ft8", "ft9", "ft10", "ft11",
     };
     char* control_flow[] =
     {
-        "uret", "sret", "mret", "wfi", "jal",  "jalr",
-        "beq",  "bne",  "blt",  "bge", "bltu", "bgeu",
+        "uret",   "sret", "mret", "wfi",  "jal",  "jalr", "beq",
+        "bne",    "blt",  "bge",  "bltu", "bgeu", "jal",  "jr",
+        "ebreak", "jalr", "beqz", "bnez", "j",
     };
     char* typenames[] =
     {
@@ -77,14 +85,14 @@ yed_plugin_boot(yed_plugin* self)
 
     yed_plugin_set_unload_fn(self, unload);
 
-    frame.kind = EVENT_FRAME_PRE_BUFF_DRAW;
-    frame.fn = syntax_rust_frame_handler;
-    line.kind = EVENT_LINE_PRE_DRAW;
-    line.fn = syntax_rust_line_handler;
-    buff_mod_pre.kind = EVENT_BUFFER_PRE_MOD;
-    buff_mod_pre.fn = syntax_rust_buff_mod_pre_handler;
+    frame.kind         = EVENT_FRAME_PRE_BUFF_DRAW;
+    frame.fn           = syntax_rust_frame_handler;
+    line.kind          = EVENT_LINE_PRE_DRAW;
+    line.fn            = syntax_rust_line_handler;
+    buff_mod_pre.kind  = EVENT_BUFFER_PRE_MOD;
+    buff_mod_pre.fn    = syntax_rust_buff_mod_pre_handler;
     buff_mod_post.kind = EVENT_BUFFER_POST_MOD;
-    buff_mod_post.fn = syntax_rust_buff_mod_post_handler;
+    buff_mod_post.fn   = syntax_rust_buff_mod_post_handler;
 
     yed_plugin_add_event_handler(self, frame);
     yed_plugin_add_event_handler(self, line);
@@ -102,7 +110,7 @@ yed_plugin_boot(yed_plugin* self)
     ARRAY_LOOP(typenames)
     highlight_add_kwd(&hinfo, *it, HL_TYPE);
     highlight_add_kwd(&hinfo, "zero", HL_CON);
-    highlight_within(&hinfo, "(", ")", 0, -1, HL_CF);
+    highlight_within(&hinfo, "(", ")", 0, -1, HL_CALL);
     highlight_numbers(&hinfo);
     highlight_within(&hinfo, "\"", "\"", '\\', -1, HL_STR);
     highlight_within(&hinfo, "'", "'", '\\', 1, HL_CHAR);
